1054f6192ed96f7cf33eef51cbf05d6d45d8e2c2
sriharshaslu1199 <sriharsha.kethavarapu@slu.edu>
2025-04-22 17:47:02 +0000

    Lab 5

--- api.js
+++ api.js
@@ -83,3 +83,68 @@ module.exports = autoCatch({
   editProduct,
   deleteProduct
 });
+/**
+ * Create a new product
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+async function createProduct (req, res, next) {
+  const product = await Products.create(req.body)
+  res.json(product)
+}
+// api.js
+
+/**
+ * Update a product
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+async function editProduct (req, res, next) {
+  const change = req.body
+  const product = await Products.edit(req.params.id, change)
+  res.json(product)
+}
+
+/**
+ * Delete a product
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+async function deleteProduct (req, res, next) {
+  const response = await Products.destroy(req.params.id)
+  res.json(response)
+}
+// api.js
+
+/**
+ * Create an order
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+async function createOrder (req, res, next) {
+  const order = await Orders.create(req.body)
+  res.json(orders)
+}
+
+/**
+ * List orders
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+async function listOrders (req, res, next) {
+  const { offset = 0, limit = 25, productId, status } = req.query
+
+  const orders = await Orders.list({ 
+    offset: Number(offset), 
+    limit: Number(limit),
+    productId, 
+    status 
+  })
+
+  res.json(orders)
+}
--- app.js
+++ app.js
@@ -21,4 +21,9 @@ app.delete('/products/:id', api.deleteProduct)
 app.post('/products', api.createProduct)
 // Boot the server
 app.listen(port, () => console.log(`Server listening on port ${port}`))
+// app.js
 
+// ...
+
+app.get('/orders', api.listOrders)
+app.get('/orders/', api.createOrder)
